ba932ebfad03012d1e51bf6d4d4971060c1c92b5
Siponek <szinkowicz@gmail.com>
2023-06-12 15:33:59 +0200

    Refactor: Changed name to PetPage for Edit and Adding (same functionalities)

--- src/test/java/POs/PetPageObject.java
+++ src/test/java/POs/PetPageObject.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.support.*;
 import org.openqa.selenium.support.ui.Select;
 
 // PageObjects/AddPetPageObject.java
-public class AddPetPageObject extends BasePagePO{
+public class PetPageObject extends BasePagePO{
 
     @FindBy(id = "name")
     private WebElement petName;
@@ -22,20 +22,35 @@ public class AddPetPageObject extends BasePagePO{
     @FindBy(xpath = "//*[text() = 'Add Pet']")
     public WebElement buttonAddPet;
 
-    public AddPetPageObject(WebDriver driver) {
+    @FindBy(xpath = "//*[text() = 'Update Pet']")
+    public WebElement buttonUpdatePet;
+
+    public PetPageObject(WebDriver driver) {
         super(driver);
     }
 
     public void providePetData(String petName, String birthDate, String type) {
-        Select petTypeDropdown = new Select(this.petTypeElement);
+        providePetName(petName);
+        providePetBirthDate(birthDate);
+        providePetType(type);
+    }
+    public void providePetName(String petName) {
         this.petName.clear();
         this.petName.sendKeys(petName);
+    }
+    public void providePetBirthDate(String birthDate) {
         this.birthDate.clear();
         this.birthDate.sendKeys(birthDate);
+    }
+    public void providePetType(String type) {
+        Select petTypeDropdown = new Select(this.petTypeElement);
         petTypeDropdown.selectByVisibleText(type);
     }
     public void clickAddPetButton() {
         buttonAddPet.click();
+    }
+    public void clickUpdatePetButton() {
+        buttonUpdatePet.click();
     }
     public String getErrorMessage() {
         return errorMessage.getText();
