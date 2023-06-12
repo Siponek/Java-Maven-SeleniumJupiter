351dd63f696055dd789c13c921a03a8295b8fb4d
Siponek <szinkowicz@gmail.com>
2023-06-12 15:34:45 +0200

    Refactor: changed PageObject used to PetPageObject

--- src/test/java/test/AddPetTest.java
+++ src/test/java/test/AddPetTest.java
@@ -1,6 +1,6 @@
 package test;
 
-import POs.AddPetPageObject;
+import POs.PetPageObject;
 import POs.FindOwnersPageObject;
 import POs.OwnerInformationPageObject;
 import io.github.bonigarcia.seljup.SeleniumJupiter;
@@ -8,27 +8,24 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.time.Duration;
 
 import static POs.BasePagePO.ANSI_BLUE;
 import static POs.BasePagePO.ANSI_RESET;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(SeleniumJupiter.class)
 public class AddPetTest {
     ChromeDriver driver;
     FindOwnersPageObject findOwnersPage;
     OwnerInformationPageObject ownerInfoPage;
-    AddPetPageObject addPetPage;
+    PetPageObject addPetPage;
 
     @BeforeEach
     public void setup(ChromeDriver driver) {
         this.driver = driver;
         findOwnersPage = new FindOwnersPageObject(driver);
         ownerInfoPage = new OwnerInformationPageObject(driver);
-        addPetPage = new AddPetPageObject(driver);
+        addPetPage = new PetPageObject(driver);
         findOwnersPage.visit("http://localhost:8080/owners/find");
     }
 
@@ -37,7 +34,7 @@ public class AddPetTest {
         findOwnersPage.searchForAllOwners();
         findOwnersPage.clickOnOwner("Franklin");
         ownerInfoPage.goToAddNewPet();
-        addPetPage.providePetData("Charles1", "05301999", "dog");
+        addPetPage.providePetData("Charles12", "05301999", "dog");
         addPetPage.clickAddPetButton();
         assertTrue(ownerInfoPage.isPetDetailsPresent("Charles"), ANSI_BLUE + "New pet is not in list" + ANSI_RESET);
         assertTrue(ownerInfoPage.getOwnerName("Franklin").contains("George Franklin"), ANSI_BLUE + "Owner name is not correct" + ANSI_RESET);
